5da5c1f2933295914d9391f7bc0ac9fd0d2f737f
CAai <c.asami@cosmic-jpn.co.jp>
2020-08-20 17:44:19 +0900

    Plotter.py:scatter function corresponds to multi class Data_processor.py:make new

---
+++ Data_processor.py
@@ -0,0 +1,37 @@
+import umap
+import pandas as pd
+
+#Compress dimention
+def dimensional_compressor(df, ignore_column=[], dimention=1, random_seed=None):
+    '''
+    Parameters
+    ----------
+    df : TYPE:pd.DataFrame
+        data.
+    ignore_column : TYPE:like array
+        Not compress columns. The default is [].
+    demention : TYPE:integer
+        Dimention after compression. The default is 1.
+    random_seed : TYPE:integer
+        Random seed. The default is None.
+
+    Returns
+    -------
+    TYPE:pd.DataFrame
+        Concatenation 'ignore_column' and compressed data.
+    '''
+    
+    #Divide the 'df' into target data and ignore data
+    if len(ignore_column)>=1:
+        target_df = df.drop(ignore_column, axis=1)
+        non_target_df = df[ignore_column]
+    
+    #Change 'dimention' to 1 if dimention of target data is greater than 'dimention'
+    if len(target_df.columns) < dimention:        
+        dimention = 1
+        print('WARNING : Compress dimention is upper than columns. -> Processed as dimention=1 .')
+    
+    compressed_np = umap.UMAP(random_state=random_seed, n_components=dimention).fit_transform(target_df)
+    compressed_df = pd.DataFrame(compressed_np)       
+    
+    return pd.concat([non_target_df, compressed_df], axis=1)
--- Plotter.py
+++ Plotter.py
@@ -43,16 +43,21 @@ def plot_beeswarm(data, x_label=None, y_label=None, x_ticklabels=[]):
 
     plt.show()
     
-#Plot scatter
-def plot_2D_scatters(data, ticklabels=None):
+#Plot scatters
+def plot_2D_scatters(data,ticklabels=None,group=None,group_name=None,display_correlation=False):
     sns.set_style("whitegrid")  
 
     data = pd.DataFrame(data, columns=ticklabels)
     
     #Define gragh size
-    divisor = sorted([i for i in range(1,len(data.columns)+1) if len(data.columns)%i == 0])
-    vertical = divisor[int(np.median(range(0,len(divisor))))]
-    horizonal = int(len(data.columns)/vertical)
+    vertical = 1
+    horizonal = 1
+    
+    #Define gragh size like square if data number is upper 3
+    if len(data.columns)>=3:
+        divisor = sorted([i for i in range(1,len(data.columns)+1) if len(data.columns)%i == 0])
+        vertical = divisor[int(np.median(range(0,len(divisor))))]
+        horizonal = int(len(data.columns)/vertical)
 
     fig, ax = plt.subplots(vertical, horizonal, figsize=(5*horizonal, 4*vertical), squeeze=False)
     
@@ -60,23 +65,62 @@ def plot_2D_scatters(data, ticklabels=None):
         hor_idx = i%len(data.columns)
         ver_idx = int(i/len(data.columns))
         
-        sns.scatterplot(x=data.iloc[:,pair[0]], y=data.iloc[:,pair[1]], ax=ax[ver_idx][hor_idx])
+        if not group is None:
+            for i in sorted(group.unique()):
+                sns.scatterplot(x=data.iloc[:,pair[0]].loc[(group==i)],
+                                y=data.iloc[:,pair[1]].loc[(group==i)],
+                                label=group_name[i],ax=ax[ver_idx][hor_idx])
+                ax[ver_idx][hor_idx].legend()
+        else:
+            sns.scatterplot(x=data.iloc[:,pair[0]], y=data.iloc[:,pair[1]], ax=ax[ver_idx][hor_idx])
         
-        corr = stats.pearsonr(data.iloc[:,pair[0]], data.iloc[:,pair[1]])[0]
-        ax[ver_idx][hor_idx].text(0.95, 0.90, f'R={round(corr,3)}', size=10, transform=ax[ver_idx][hor_idx].transAxes,
-                                  horizontalalignment = 'right', bbox=dict(facecolor='white', edgecolor='black'))
+        if display_correlation:
+            corr = stats.pearsonr(data.iloc[:,pair[0]], data.iloc[:,pair[1]])[0]
+            ax[ver_idx][hor_idx].text(0.95, 0.90, f'R={round(corr,3)}', size=10, transform=ax[ver_idx][hor_idx].transAxes,
+                                      horizontalalignment = 'right', bbox=dict(facecolor='white', edgecolor='black'))
      
-    plt.show()  
+    plt.show()
     
   
 #Plot 3D scatter
-def plot_3D_scattaer(x,y,z,x_label=None,y_label=None,z_label=None):
+def plot_3D_scattaer(x,y,z,x_label=None,y_label=None,z_label=None,group=None,group_name=None):
+    '''  
+    Parameters
+    ----------
+    x : TYPE:like array
+        First demention data.
+    y : TYPE:like array
+        Second demention data.
+    z : TYPE:like array
+        Third demention data.
+    x_label : TYPE:str
+        First demention name. The default is None.
+    y_label : TYPE:str
+        Seconde demention name. The default is None.
+    z_label : TYPE:str
+        Third demention name. The default is None.
+    group : TYPE:like array
+        Data group (like class). The default is None.
+    group_name : TYPE:dict
+        Corresponds to the group name. The default is None.
+
+    Returns
+    -------
+    None.
+    '''
+    
     sns.set_style("whitegrid") 
 
     fig = plt.figure()
     ax = Axes3D(fig)
 
-    ax.plot(x,y,z,marker="o",linestyle='None')
+    if not group is None:
+        for i in sorted(group.unique()):
+            ax.plot(x[group==i],y[group==i],z[group==i],marker=".",linestyle='None',
+                    label=group_name[i])
+        ax.legend()
+    else:
+        ax.plot(x,y,z,marker="o",linestyle='None')
     
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
