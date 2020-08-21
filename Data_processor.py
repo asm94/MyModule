0600c291921a2d99b850b8ba9f594c32c3b2dfe5
CAai <c.asami@cosmic-jpn.co.jp>
2020-08-21 14:35:56 +0900

    Data_processor.py::Corresponds to various index pattern @dimensional_compressor

--- Data_processor.py
+++ Data_processor.py
@@ -23,8 +23,8 @@ def dimensional_compressor(df, ignore_column=[], dimention=1, random_seed=None):
     
     #Divide the 'df' into target data and ignore data
     if len(ignore_column)>=1:
-        target_df = df.drop(ignore_column, axis=1)
-        non_target_df = df[ignore_column]
+        target_df = df.drop(ignore_column, axis=1).reset_index(drop=True)
+        non_target_df = df[ignore_column].reset_index(drop=True)
     
     #Change 'dimention' to 1 if dimention of target data is greater than 'dimention'
     if len(target_df.columns) < dimention:        
@@ -32,6 +32,6 @@ def dimensional_compressor(df, ignore_column=[], dimention=1, random_seed=None):
         print('WARNING : Compress dimention is upper than columns. -> Processed as dimention=1 .')
     
     compressed_np = umap.UMAP(random_state=random_seed, n_components=dimention).fit_transform(target_df)
-    compressed_df = pd.DataFrame(compressed_np)       
+    compressed_df = pd.DataFrame(compressed_np)
     
     return pd.concat([non_target_df, compressed_df], axis=1)
